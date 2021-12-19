093fbaf244745cd878ef9051de8ed425a0fb15ca
generall3 <67384202+generall3@users.noreply.github.com>
2021-12-19 19:53:43 +0300

    Update Vote.ts

---
+++ tasks/DAO/Vote.ts
@@ -0,0 +1,29 @@
+import { ExcaliburToken } from "./../../typechain-types/CRGToken";
+import { ExcaliburToken__factory } from "./../../typechain-types/factories/CRGToken__factory";
+import { Console } from "console";
+import { DAO } from "./../../typechain-types/DAO";
+import { DAO__factory } from "./../../typechain-types/factories/DAO__factory";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import Web3 from "web3";
+import { ethers } from "ethers";
+import { getConfig } from "../../utils/networks";
+import { task } from "hardhat/config";
+
+task("vote", "Do vote for proposal", async (args, hre) => {
+  const [owner, addr1] = await hre.ethers.getSigners();
+  const networkName = hre.network.name;
+  const web3 = new Web3(Web3.givenProvider);
+  console.log(`networkName`, networkName);
+  const { tokenName, tokenAddress, tokenSymbol, tokenDecimals } =
+    getConfig(networkName);
+
+  let Token: ExcaliburToken__factory;
+  let token: ExcaliburToken;
+  if (networkName === "rinkeby") {
+    Token = await hre.ethers.getContractFactory("ExcaliburToken");
+    token = await Token.attach(tokenAddress);
+  } else {
+    Token = await hre.ethers.getContractFactory("ExcaliburToken");
+    token = await Token.deploy(tokenName, tokenSymbol, tokenDecimals);
+  }
+});
